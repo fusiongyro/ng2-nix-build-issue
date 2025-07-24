cd6c1a035c3bf7464534bc35c58fde48a241e16b
Daniel K Lyons <fusion@storytotell.org>
2025-07-24 13:01:24 -0600

    Use yarn instead of npm

--- flake.nix
+++ flake.nix
@@ -14,35 +14,43 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = pkgs.buildNpmPackage {
-          name = "nglearn";
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "nglearn";
+          version = "0.0.0";
 
           src = gitignore.lib.gitignoreSource ./.;
 
-          npmDeps = pkgs.importNpmLock.buildNodeModules {
-            npmRoot = ./.;
-            nodejs = pkgs.nodejs;
+          yarnOfflineCache = pkgs.fetchYarnDeps {
+            yarnLock = ./yarn.lock;
+            hash = "sha256-L/0WeJxR/KIY5fV5AKSutnzkvnOC4ZtkSCcedZo2Ijw=";
           };
 
-          npmFlags = [ "--legacy-peer-deps" ];
+          nativeBuildInputs = [
+            pkgs.yarnConfigHook
+            pkgs.yarnBuildHook
+            pkgs.yarnInstallHook
+            pkgs.nodejs
+            pkgs.yarn
+          ];
 
-          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+          dontYarnInstall = true;
 
           installPhase = ''
-              mkdir $out
-              cp -r dist/nglearn/browser/* $out
-            '';
+            mkdir $out
+            cp -r dist/nglearn/browser/* $out
+          '';
         };
 
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.importNpmLock.hooks.linkNodeModulesHook ];
-
-          npmDeps = pkgs.importNpmLock.buildNodeModules {
-            npmRoot = ./.;
-            nodejs = pkgs.nodejs;
+          yarnOfflineCache = pkgs.fetchYarnDeps {
+            yarnLock = ./yarn.lock;
+            hash = "sha256-L/0WeJxR/KIY5fV5AKSutnzkvnOC4ZtkSCcedZo2Ijw=";
           };
 
-          buildInputs = with pkgs; [ nodejs ];
+          buildInputs = with pkgs; [ nodejs yarn ];
+          nativeBuildInputs = [ pkgs.yarnConfigHook ];
+
+          shellHook = "yarn install";
         };
       };
     };
